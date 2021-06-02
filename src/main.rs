01e8c67a4cca560d84462a33a6640ff811b88e96
Usairim Isani <usairim.isani@outlook.com>
2021-06-02 21:02:25 +0500

    Debugged Program counter manipulation.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::{
     collections::HashMap,
     hash::Hash,
-    ops::{Add, Mul},
+    ops::{Add, Div, Mul, Sub},
 };
 
 #[derive(Debug)]
@@ -18,9 +18,9 @@ pub enum Instruction<T, R> {
     MULTIPLY,
 
     RETURN_VALUE,
-    LOOP(R),
-    JUMP,
-    IF_NOT_EQUAL(T),
+    LOOP(usize),
+    JUMP(usize),
+    IF_NOT_EQUAL,
 }
 impl<T, R> Instruction<T, R> {
     fn parse(s: &str) -> Instruction<T, R> {
@@ -61,9 +61,18 @@ impl<T, R> Interpreter<T, R> {
     fn interpret(&mut self) {}
 }
 fn interpreter<T, R>(byte_code: &[ByteCode<T, R>]) -> Option<T>
+// fn interpreter(byte_code: &[ByteCode<i32, &str>]) -> Option<i32>
 where
     // Solved an issue regarding pushing to vec. https://stackoverflow.com/questions/37647248/mismatched-types-when-returning-the-result-of-adding-two-generics
-    T: std::fmt::Debug + Default + Add<Output = T> + Mul<Output = T> + Copy,
+    T: std::fmt::Debug
+        + Default
+        + Add<Output = T>
+        + Div<Output = T>
+        + Mul<Output = T>
+        + Sub<Output = T>
+        + Copy
+        + Eq
+        + PartialEq,
     R: std::fmt::Debug + Default + Eq + Hash + Copy,
 {
     // A Program Counter to keep track of current execution.
@@ -71,11 +80,16 @@ where
 
     // Stack to store temp vars.
     let mut stack: Vec<T> = Vec::new();
+    // let mut stack: Vec<i32> = Vec::new();
 
     // A look up table to keep track of variables
     let mut map: HashMap<R, T> = HashMap::new();
+    // let mut map: HashMap<&str, i32> = HashMap::new();
 
     let mut r = None;
+
+    let mut current_iter = 0;
+    let mut loop_limit = 0;
 
     while prog_count < byte_code.len() {
         // Advance the program counter by
@@ -84,13 +98,14 @@ where
         match byte_code[prog_count].instruction {
             Instruction::LOAD_VAL(val) => {
                 // Put temp var into stack
+                // println!("Stack in LOAD_VAL: {:?}\n", stack);
                 stack.push(val);
             }
             Instruction::WRITE_VAR(key) => {
                 // write to the lookup table from the stack
                 let val = stack.pop().unwrap();
                 map.insert(key, val);
-                println!("Map : {:?}", map);
+                // println!("Map : {:?}", map);
             }
 
             Instruction::READ_VAR(key) => {
@@ -100,6 +115,7 @@ where
             }
             Instruction::ADD => {
                 // Perform Operation 'ADD'on 'a' and temp val in the stack
+                // println!("Stack in ADD: {:?}\n", stack);
                 if let Some((a, b)) = stack.pop().zip(stack.pop()) {
                     stack.push(a + b);
                 }
@@ -114,15 +130,25 @@ where
             Instruction::RETURN_VALUE => {
                 r = stack.pop();
             }
-            Instruction::LOOP(key) => {
-                // map.insert("limit", prog_count);
-                // map.insert(key, prog_count);
+            Instruction::LOOP(l) => {
+                // map.insert(key, loop_limit);
+                // println!("Stack in LOOP: {:?}\n", stack);
+                loop_limit = l;
+                current_iter = 0;
             }
-            Instruction::JUMP => {
-                // let loop_index = map.get(key).unwrap();
-                // prog_count = loop_index as us;
+            Instruction::JUMP(p_c) => {
+                // println!("Stack in JUMP: {:?}\n", stack);
+                prog_count = prog_count - p_c - 1;
+                current_iter += 1;
             }
-            Instruction::IF_NOT_EQUAL(limit) => {}
+            Instruction::IF_NOT_EQUAL => {
+                // println!("Current : {:?}", current_iter);
+                // println!("Stack : in IFNOT{:?}\n", stack);
+                // println!("Map  : {:?}", map);
+                if loop_limit == current_iter {
+                    adv_pc = 2;
+                }
+            }
         }
         prog_count += adv_pc;
     }
@@ -174,5 +200,60 @@ mod tests {
         let r = interpreter(&b_code);
         println!("{:?}", r);
         assert_eq!(None, r);
+    }
+
+    #[test]
+    fn read_var() {
+        let b_code: Vec<ByteCode<i32, &str>> = vec![
+            ByteCode {
+                instruction: Instruction::LOAD_VAL(2),
+            },
+            ByteCode {
+                instruction: Instruction::WRITE_VAR("x"),
+            },
+            ByteCode {
+                instruction: Instruction::READ_VAR("x"),
+            },
+            ByteCode {
+                instruction: Instruction::RETURN_VALUE,
+            },
+        ];
+        let r = interpreter(&b_code);
+
+        println!("{:?}", r);
+
+        assert_eq!(Some(2), r);
+    }
+
+    #[test]
+    fn loop_prog() {
+        let b_code: Vec<ByteCode<i32, &str>> = vec![
+            ByteCode {
+                instruction: Instruction::LOAD_VAL(2),
+            },
+            ByteCode {
+                instruction: Instruction::LOOP(1),
+            },
+            ByteCode {
+                instruction: Instruction::LOAD_VAL(2),
+            },
+            ByteCode {
+                instruction: Instruction::ADD,
+            },
+            ByteCode {
+                instruction: Instruction::IF_NOT_EQUAL,
+            },
+            ByteCode {
+                instruction: Instruction::JUMP(3),
+            },
+            ByteCode {
+                instruction: Instruction::RETURN_VALUE,
+            },
+        ];
+        let r = interpreter(&b_code);
+
+        println!("{:?}", r);
+
+        assert_eq!(Some(6), r);
     }
 }
